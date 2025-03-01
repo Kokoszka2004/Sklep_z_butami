a817f0c994bd5464ccbce1249dc363c46aa40b98
Kokoszka <153829780+Kokoszka2004@users.noreply.github.com>
2025-03-01 19:37:50 +0100

    Delete OstatecznePodsumowanie.cpp

--- OstatecznePodsumowanie.cpp
+++
@@ -1,9 +0,0 @@
-#include "OstatecznePodsumowanie.h"
-
-// Konstruktor
-OstatecznePodsumowanie::OstatecznePodsumowanie(double kwota) : kwota(kwota) {}
-
-// Oblicza laczna cene
-double OstatecznePodsumowanie::obliczLacznaCene() const {
-    return kwota;
-}
