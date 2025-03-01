947851a165e4ab9c993debc81ea45c437af10798
Kokoszka <153829780+Kokoszka2004@users.noreply.github.com>
2025-03-01 19:37:28 +0100

    Delete Konto.cpp

--- Konto.cpp
+++
@@ -1,6 +0,0 @@
-#include "Konto.h"
-
-// Konstruktor
-Konto::Konto(const std::string& nazwa, const std::string& haslo, const std::string& email)
-    : nazwaUzytkownika(nazwa), haslo(haslo), email(email) {
-}
